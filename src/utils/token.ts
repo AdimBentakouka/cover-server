435155e1cb2b0fba74998902beabacf7dcfae9f0
Adim <adim.bentakouka@gmail.com>
2022-01-08 11:47:18 +0100

    Fix bug: l'id n'étais plus présent lors de la régénération du token

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -3,103 +3,89 @@ import { Request } from "express";
 
 import crypto from "crypto";
 
-import {Session, User} from "../models/";
-import {JsonToken} from "../types/token";
-
-
-
+import { Session, User } from "../models/";
+import { JsonToken } from "../types/token";
 
 // Génère un token access
-export function generateToken(user: {id: string, email: string, name: string, isAdmin?: boolean}) : string
-{
-     let jsonToken: JsonToken = {
-          id: user.id,
-          email: user.email,
-          username: user.name
-     };
+export function generateToken(user: { id: string; email: string; name: string; isAdmin?: boolean }): string {
+	let jsonToken: JsonToken = {
+		id: user.id,
+		email: user.email,
+		username: user.name,
+	};
 
-     if (user.isAdmin) {
-          jsonToken = { ...jsonToken, isAdmin: user.isAdmin };
-     }
+	if (user.isAdmin) {
+		jsonToken = { ...jsonToken, isAdmin: user.isAdmin };
+	}
 
-     return jwt.sign(jsonToken, process.env.JWT_KEY_TOKEN, {
-          expiresIn: process.env.JWT_EXPIRE_TOKEN
-     });
+	return jwt.sign(jsonToken, process.env.JWT_KEY_TOKEN, {
+		expiresIn: process.env.JWT_EXPIRE_TOKEN,
+	});
 }
 
 //  Génére et stock un refreshToken
-export function generateRefreshToken(userId: string, req: Request) : Promise<string>
-{
-     return new Promise<string>((resolve, reject) => {
-          // Créer une clée unique
-          const refreshToken = crypto.randomBytes(64).toString("hex");
+export function generateRefreshToken(userId: string, req: Request): Promise<string> {
+	return new Promise<string>((resolve, reject) => {
+		// Créer une clée unique
+		const refreshToken = crypto.randomBytes(64).toString("hex");
 
-          const browser = req.useragent?.browser || "unknown";
-          const os = req.useragent?.os || "unknown";
-          const date = new Date();
-          // Enregistrer la clé en bdd avec l'id et les info du client
+		const browser = req.useragent?.browser || "unknown";
+		const os = req.useragent?.os || "unknown";
+		const date = new Date();
+		// Enregistrer la clé en bdd avec l'id et les info du client
 
-          Session.create({
-               refreshToken: refreshToken,
-               browser: browser,
-               os: os,
-               userId: userId,
-               lastUsage: date
-          })
-          .then(() => {
-               resolve(refreshToken);
-          })
-          .catch((err) => {
-               console.log(err);
-               reject({msg: "Session creation failed"});
-          });
-
-     });
-     
-
-     
+		Session.create({
+			refreshToken: refreshToken,
+			browser: browser,
+			os: os,
+			userId: userId,
+			lastUsage: date,
+		})
+			.then(() => {
+				resolve(refreshToken);
+			})
+			.catch((err) => {
+				console.log(err);
+				reject({ msg: "Session creation failed" });
+			});
+	});
 }
 
 // Générer un nouveau token à partir d'un refreshToken
-export function generateNewToken(refreshToken: string) : Promise<string>
-{
-     return new Promise<string>((resolve, reject) => {
-          Session.findOne({
-               attributes: ["id", "lastUsage"],
-               include: [{
-                    attributes: ["name", "email", "isAdmin", "accountValid"],
-                    model: User
-               }],
-               where : {
-                    refreshToken: refreshToken
-               }
-     
-          }).then((_session) => {
+export function generateNewToken(refreshToken: string): Promise<string> {
+	return new Promise<string>((resolve, reject) => {
+		Session.findOne({
+			attributes: ["id", "lastUsage"],
+			include: [
+				{
+					attributes: ["name", "email", "isAdmin", "accountValid"],
+					model: User,
+				},
+			],
+			where: {
+				refreshToken: refreshToken,
+			},
+		})
+			.then((_session) => {
+				if (_session) {
+					_session.lastUsage = new Date();
+					_session.save();
 
-               if(_session)
-               {
-                    _session.lastUsage = new Date();
-                    _session.save();
-
-                    resolve(generateToken({
-                         id: _session.user.id,
-                         email: _session.user.email,
-                         name: _session.user.name,
-                         isAdmin: _session.user.isAdmin,
-                    }));
-               }
-               else
-               {
-                    reject("session introuvable");
-               }
-         
-          })
-          .catch((err) => {
-               console.log(err);
-               reject(err);
-          });
-     });
-
+					resolve(
+						generateToken({
+							id: _session.userId,
+							email: _session.user.email,
+							name: _session.user.name,
+							isAdmin: _session.user.isAdmin,
+						})
+					);
+				} else {
+					reject("session introuvable");
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+				reject(err);
+			});
+	});
 }
-
-
