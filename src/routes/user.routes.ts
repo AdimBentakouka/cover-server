9b1a790e3eb9b1a88975c81ba0d8fc4f63735e87
Adim <adim.bentakouka@gmail.com>
2022-05-02 01:26:54 +0200

    Route Admin

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,7 @@
 import { Router } from "express";
 
-
 import * as userController from "../controllers/user.controllers";
 import * as authMiddleware from "../middlewares/auth.middleware";
-
 
 const userRouter = Router();
 
@@ -12,6 +10,6 @@ userRouter.post("/login", userController.login);
 userRouter.get("/refreshtoken", userController.refreshToken);
 userRouter.get("/admin/validaccount/:userid", authMiddleware.authentificate, userController.validaccount);
 userRouter.get("/admin/getusers/", authMiddleware.authentificate, userController.getAllAccount);
-
+userRouter.get("/admin/delete/:id", authMiddleware.authentificate, userController.deleteAccount);
 
 export default userRouter;
